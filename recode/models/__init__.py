e68a7a588485ebef32e23a9848b963da52c72321
riverfjs <riverfjs@gmail.com>
2021-05-03 13:40:12 +0000

    delete recode

--- recode/models/__init__.py
+++
@@ -1,6 +0,0 @@
-from __future__ import absolute_import
-
-from .bottleneck_transformer_pytorch import BottleStack, BottleBlock
-from .resnext_wsl import *
-from .btresnet import *
-from .botnet import *
